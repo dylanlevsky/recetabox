3573e11e904bd371c0e51aeb7f4249cc98ba9f5b
Dylan Moguilevsky <levsky.dylan@gmail.com>
2019-07-23 17:33:44 -0300

    enviroment change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
-export const environment = {
+
+/*export const environment = {
   production: true
+};
+*/
+
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCzZYJVmKSl2MhNcPwNfQ80eX9sctwtRwE",
+    authDomain: "recetabox.firebaseapp.com",
+    databaseURL: "https://recetabox.firebaseio.com",
+    projectId: "recetabox",
+    storageBucket: "recetabox.appspot.com",
+    messagingSenderId: "155928434306",
+    appId: "1:155928434306:web:4f01ae1e4be97d19"
+  }
 };
